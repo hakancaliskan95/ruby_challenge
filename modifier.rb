37d2522e754e91efe38a987a17e81a16907b2228
hakan <hakancaliskan95@gmail.com>
2018-03-20 17:55:32 +0300

    float and stirngs
    float and stirngs class moved to a new file

--- modifier.rb
+++ modifier.rb
@@ -18,17 +18,6 @@ def latest(name)
   files.last
 end
 
-class String
-  def from_german_to_f
-    self.gsub(',', '.').to_f
-  end
-end
-
-class Float
-  def to_german_s
-    self.to_s.gsub('.', ',')
-  end
-end
 
 class Modifier
 
